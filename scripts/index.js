b5c2741dc9202922694151e4fa85c4a6c0938190
savannah-borst <savannah.borst@gmail.com>
2022-03-07 20:18:10 +0100

    get elements set up

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+//Get elements by ID
+//amounts
+const balanceAmount = document.getElementById("balance-amount");
+const loanAmount = document.getElementById("loan-amount");
+const workAmount = document.getElementById("work-amount");
+
+//buttons
+const getLoanBtn = document.getElementById("get-loan");
+const repayLoanBtn = document.getElementById("repay-loan");
+const workBtn = document.getElementById("work");
+const bankBtn = document.getElementById("bank");
+const buyBtn = document.getElementById("buy-laptop");
+
+//Laptop
+const features = document.getElementById("features");
+const image = document.getElementById("laptop-image");
+const name = document.getElementById("laptop-name");
+const info = document.getElementById("laptop-info");
+const cost = document.getElementById("laptop-cost");
