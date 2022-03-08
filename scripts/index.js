b479313081262fc946f7254fcaf5413b4162a1e1
savannah-borst <savannah.borst@gmail.com>
2022-03-08 09:52:03 +0100

    basic bank function

--- scripts/index.js
+++ scripts/index.js
@@ -18,11 +18,16 @@ const name = document.getElementById("laptop-name");
 const info = document.getElementById("laptop-info");
 const cost = document.getElementById("laptop-cost");
 
+//start
 let workMoney = 0;
 workAmount.innerHTML = workMoney + " " + "Euro";
 
+let bankMoney = 0;
+balanceAmount.innerHTML = bankMoney + " " + "Euro";
+
 //event listeners
 workBtn.addEventListener("click", work);
+bankBtn.addEventListener("click", bank);
 
 //functions
 function work() {
@@ -31,7 +36,11 @@ function work() {
 }
 
 function bank() {
-    
+    bankMoney += workMoney
+    balanceAmount.innerHTML = bankMoney + " " + "Euro";
+
+    workMoney = 0;
+    workAmount.innerHTML = workMoney + " " + "Euro";
 }
 
 function getLoan() {
